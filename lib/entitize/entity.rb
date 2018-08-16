c82f28b98d5135d33c1971c1babba5c6b6deb78a
stephenfiser <stephen.fiser@gmail.com>
2018-08-16 11:45:13 -0500

    removes old gems

--- lib/entitize/entity.rb
+++ lib/entitize/entity.rb
@@ -1,25 +1,5 @@
 module Entitize
   class Entity
-    class << self
-
-      def generate(data, class_name)
-        Entitize::Classifier.generate(data, class_name)
-      end
-
-      def auto_new(*args, &block)
-        instance = allocate
-        instance.auto_initialize(*args, &block)
-        instance
-      end
-
-    end # --> END CLASS METHODS
-
-    def initialize(data)
-      Entitize::Classifier.define_methods(data, self)
-    end
-
-    def auto_initialize(data)
-      Entitize::Classifier.define_methods(data, self)
-    end
+    include Entitize::Entitizable
   end
 end
