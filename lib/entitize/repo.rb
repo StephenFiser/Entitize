465304bbd214c29bb3ffb3ec2d899e8f5a15c79e
stephenfiser <stephen.fiser@gmail.com>
2018-08-20 11:00:47 -0500

    adds comment

--- lib/entitize/repo.rb
+++ lib/entitize/repo.rb
@@ -13,6 +13,7 @@ module Entitize
       arguments         = options[:args] || []
       class_name_to_use = options[:entity] || data_source_class.to_s
 
+      # TODO: add in all settings as first args -> e.g. pub_key, secret_key
       arguments.unshift(token) unless token.nil?
       data = data_source_class.send(query, *arguments)
 
