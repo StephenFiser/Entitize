4ee65eeccb38478ce346c4d320bb545d6f8be31e
Stephen Fiser <stephen@Stephens-MacBook-Pro.local>
2019-01-17 13:28:42 -0600

    can now configure base class name

--- lib/entitize.rb
+++ lib/entitize.rb
@@ -10,10 +10,28 @@ module Entities
 end
 
 module Entitize
+  class Configuration
+    attr_accessor :base_class
 
-  # TODO: Make this customizable
-  def self.base_class
-    Entities
+    def initialize
+      @base_class = Entities
+    end
+  end
+
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def set_base_class(base_class)
+      configuration.base_class = base_class
+    end
+
+    def base_class
+      configuration.base_class
+    end
   end
 
 end
--- lib/entitize/version.rb
+++ lib/entitize/version.rb
@@ -1,3 +1,3 @@
 module Entitize
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
